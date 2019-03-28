cf6d8a196f802472a116f93c303b0d040b5bfa5a
Matthew Schwartz <schwartz_matthew2@bah.com>
2019-03-28 14:41:45 -0400

    Function working

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import boto3
 import datetime
 import json
+import queue
 from decimal import Decimal
-#from odevalidator import TestCase
+from odevalidator import TestCase
 
 ### Data source config
 CONFIG_FILE = "./bsmLogDuringEvent.ini"
@@ -22,19 +23,20 @@ def lambda_handler(event, context):
     ddate = datetime.datetime.now()
     prefix_string = "%s/%s/%s/%s/%s" % (DATA_PROVIDER, MESSAGE_TYPE, ddate.year, str(ddate.month).zfill(2), str(ddate.day).zfill(2))
 
-    file_list = list_s3_files_matching_prefix(prefix_string)
+    s3_file_list = list_s3_files_matching_prefix(s3_client, prefix_string)
 
     msg_queue = queue.Queue()
     for filename in s3_file_list:
         record_list = extract_records_from_file(s3_client, filename)
         [msg_queue.put(record) for record in record_list]
 
-    print(list(q.queue))
+
+    # print(list(msg_queue.queue))
 
     test_case = TestCase(CONFIG_FILE)
-    validation_results = test_case.validate(msg_queue)
+    validation_results = test_case.validate_queue(msg_queue)
 
-    if list_contains_no_errors(validation_results):
+    if list_contains_no_errors(validation_results['Results']):
         print("No validation errors detected.")
     else:
         print("Validation failed, errors detected.")
@@ -49,7 +51,7 @@ def lambda_handler(event, context):
 def send_report(message):
     ses_client = boto3.client('ses')
     response = ses_client.send_email(
-        Source=SOURCE_EMAIL
+        Source=SOURCE_EMAIL,
         Destination={
             'ToAddresses': [
                 DESTINATION_EMAIL,
@@ -81,12 +83,11 @@ def send_report(message):
 ###
 def list_contains_no_errors(result_list):
     for result in result_list:
+        print(json.dumps(result))
         for validation in result['Validations']:
             if validation['Valid'] == False:
                 return False
     return True
-
-
 
 ### Returns a list of records from a given file
 def extract_records_from_file(s3_client, filename):
