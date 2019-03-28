ebb06b5a30e7239c11990b0e717c8c97a109a3ec
Matthew Schwartz <schwartz_matthew2@bah.com>
2019-03-28 15:51:47 -0400

    Change to more appropriate zip filename

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir package
 pip install git+https://github.com/usdot-jpo-ode/ode-output-validator-library.git --upgrade --target ./package
-zip -r function.zip main.py package/
+zip -r odevalidator.zip main.py package/
 echo "Lambda package created in function.zip"
