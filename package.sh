a7658f81447309623c5652e4e909d6e5574d28ab
Matthew Schwartz <schwartz_matthew2@bah.com>
2019-06-20 21:11:34 -0400

    Update support files for SQS publisher mode

--- package.sh
+++ package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt --upgrade --target .
-zip -r canary.zip main.py slacker.py odevalidator*/
+ls odevalidator
+zip -r canary.zip main.py slacker.py odevalidator*/ sqs_client/
 rm -rf odevalidator*/
 echo "Created package in canary.zip"
