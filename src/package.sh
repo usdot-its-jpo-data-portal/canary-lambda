c1f0c1a3c280791a9410cc67980d96d912437b0f
mvs5465 <mvs5465@gmail.com>
2020-03-11 11:31:03 -0400

    Move source code files into src subdir

--- src/package.sh
+++ src/package.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# NOTE!
+# This script is deprecated and was intended for manual deployment packaging,
+# Refer to the Dockerfile of this repository for the updated deployment process.
+
 pip install -r requirements.txt --upgrade --target .
 ls odevalidator
 zip -r canary.zip main.py slacker.py odevalidator*/ sqs_client/
